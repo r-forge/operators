babb5eec75fd9a4096c2a06dd612e3507f6e126a
romain (R Enthusiast ) <francoisromain@free.fr>
2008-10-22 16:15:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/operators@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/operators/inst/options/options.R
@@ -0,0 +1,6 @@
+options( operators.regexpr = "ep" )
+options( operators.gsub = "epg" )
+options( operators.strsplit = "ep" )
+options( operators.print = "print" )
+options( operators.o.nomatch = "" )
+
