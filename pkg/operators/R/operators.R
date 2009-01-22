0feb8a34e988ec98abd8055c3a6f800573eec5e3
romain (R Enthusiast ) <francoisromain@free.fr>
2009-01-22 18:12:39 +0000

    getting ready for release
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/operators@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/operators/R/operators.R
+++ pkg/operators/R/operators.R
@@ -4,14 +4,21 @@
   x[ !x %in% table ]
 }
 
-`%x=%` <- function(x, ntimes){
-	if(ntimes == 0) x %x=% 0 else paste( rep(x, ntimes), collapse = "" ) 
+`%x=%` <- function(txt, n){
+	strrep( txt, n = n ) 
 }
 
-`%x=|%` <- function(x, ntimes){
-  if(ntimes == 0) ntimes <- getOption( "width" ) 
-	out <- paste( rep(x, ceiling( ntimes / nchar(x)  )) , collapse = "" )  
-  substr( out, 1, ntimes)
+`%x=|%` <- function(txt, length.out){
+  strrep( txt, length.out = length.out )
+}
+
+strrep <- function( txt, n, length.out = getOption("width") ){
+	if( !missing( n ) ){
+		paste( rep( txt, n ), collapse = "" )
+	} else {
+		out <- paste( rep(txt, ceiling( length.out / nchar(txt)  )) , collapse = "" )
+		substr( out, 1, length.out)
+	}
 }
 
 `%of%` <- function(x,y){
